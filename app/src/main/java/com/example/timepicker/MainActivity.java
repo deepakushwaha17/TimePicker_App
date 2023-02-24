2187988b745cb58d9e1c445bda1719ddeb41e6f2
deepakushwaha17 <deepakushwaha155@gmail.com>
2023-02-23 21:27:35 -0800

    pick time

---
+++ app/src/main/java/com/example/timepicker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.timepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+    TimePicker timePicker;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        timePicker = findViewById(R.id.time);
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+           public void onTimeChanged(TimePicker timePicker, int hour, int min) {
+                textView.setText("Hour: " + hour +"\nMinute" + min);
+           }
+       });
+    }
+}
